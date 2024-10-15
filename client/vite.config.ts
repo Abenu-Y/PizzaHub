33147790066c5a3aa67321933c0ee66964983853
Abenezer Y <154319744+Abenu-Y@users.noreply.github.com>
2024-10-15 16:46:29 +0300

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    host: '0.0.0.0', // Listen on all interfaces
+    port: process.env.PORT ? Number(process.env.PORT) : 3000, // Default port if PORT is not set
+  },
 })
