7b62da4e9a126e82835849846af89b0889a1bda9
Abenezer Y <154319744+Abenu-Y@users.noreply.github.com>
2024-10-15 17:23:23 +0300

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,5 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+   build: {
+    // Set chunking strategy to optimize memory usage
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+  },
+  server: {
+    host: '0.0.0.0', // Ensure it's accessible externally
+    port: process.env.PORT ? Number(process.env.PORT) : 3000,
+  },
  
 })
