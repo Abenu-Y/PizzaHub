a4430bf1deeb15c6c94b12445e18290d492c6bf0
Abenezer Y <154319744+Abenu-Y@users.noreply.github.com>
2024-10-15 15:59:58 +0300

    Update db.config.js

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -5,11 +5,11 @@ const { Pool } = require('pg');
 // Prepare the db parameters
 const dbConfig = {
     connectionString: process.env.DATABASE_URL,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD, 
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT || 5432, // Default port for PostgreSQL is 5432
+    // host: process.env.DB_HOST,
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD, 
+    // database: process.env.DB_DATABASE,
+    // port: process.env.DB_PORT || 5432, // Default port for PostgreSQL is 5432
     max: 10, // Connection limit
     idleTimeoutMillis: 30000, // 30 seconds before closing idle clients
     ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
